dfa32697dafed0b4bfae9021ee5e8a8c136c0564
Trần Ngọc Quân <quan31794@gmail.com>
2021-12-18 12:18:30 +0700

    homework-java01

--- lecture009-java01/Java-homework-01/src/Bmi.java
+++ lecture009-java01/Java-homework-01/src/Bmi.java
@@ -4,20 +4,20 @@ public class Bmi {
 
         method1();
 
-
+        //Trả kết quả phương thức 2
         double bmi2=method2();
-        System.out.println("Cách 2: Kiểu trả về void");
+        System.out.println("Cách 2: Kiểu trả về double");
         System.out.println("Chỉ số bmi của bạn: "+bmi2+"\n");
 
-
+       // Trả kết quả phương thức 3
         double chieucao=1.68;
         double cannang=60.5;
         double bmi3=method3(chieucao,cannang);
-        System.out.println("Cách 3: Kiểu trả về void");
+        System.out.println("Cách 3: Kiểu trả về double và tham số");
         System.out.println("Chỉ số bmi của bạn: "+bmi3+"\n");
 
 
-
+        System.out.println("So sánh: Cách trả dữ liệu sử dụng truyền tham số là tối ưu nhất do có thể linh hoạt nhập dữ liệu từ bên ngoài phương thức");
     }
     //Phương thức BMI có kiểu trả về là void
     public static void method1(){
@@ -27,7 +27,6 @@ public class Bmi {
         System.out.println("Cách 1: Kiểu trả về void");
         System.out.println("Chiều cao: "+chieucao);
         System.out.println("Cân nặng: "+cannang);
-
         System.out.println("Chỉ số bmi của bạn: "+bmi1+"\n");
 
     }
@@ -35,9 +34,9 @@ public class Bmi {
      //Phương thức BMI có kiểu trả về là double
 
      public static double method2(){
-         double chieucao=1.68;
-         double cannang=60.5;
-         return cannang/(chieucao*chieucao);
+         double height=1.68;
+         double weight=60.5;
+         return weight/(height*height);
 
 
 
