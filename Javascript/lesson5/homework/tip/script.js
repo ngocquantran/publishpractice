2c87d1759cdca3f2e3a6beaa0c6e3f537a54d37e
Trần Ngọc Quân <quan31794@gmail.com>
2022-03-19 10:09:53 +0700

    tip

---
+++ Javascript/lesson5/homework/tip/script.js
@@ -0,0 +1,43 @@
+$(function () {
+    const $billAmount = $('.amount-input');
+    const $guestNumber = $('.guests-input');
+    const $quality = $('.selected-quality');
+    const $tip = $('.tip');
+    const $calculate = $('.calculate');
+    const $result = $('.result');
+    const $selection = $('.selection');
+
+    function getTip() {
+        let billAmount = document.querySelector(".amount-input").value;
+        let guestNumber = document.querySelector(".guests-input").value;
+        // let percentage = document.querySelector(".selected-quality").value;
+        let percentage = document.querySelector(".selection").value;
+        
+        
+        billAmount = parseFloat(billAmount);
+        guestNumber = parseFloat(guestNumber);
+        percentage = parseFloat(percentage) / 100;
+        
+        
+
+        
+        if (!isNaN(billAmount) && !isNaN(guestNumber) && !isNaN(percentage)) {
+            if (Number.isInteger(guestNumber) && guestNumber > 0 && billAmount > 0) {
+            let tip;
+              tip = ((billAmount * percentage) / guestNumber).toFixed(2);
+                $tip.text("$" + tip);
+                console.log(tip);     
+            } else {
+              $result.text("Wrong input. Please check again");
+            }
+        }
+        
+           
+        
+        
+    }
+    getTip();
+
+    $calculate.on('click', getTip);
+
+})
