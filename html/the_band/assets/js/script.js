0af611c436495ad834bc65224830a6133f477c90
Trần Ngọc Quân <quan31794@gmail.com>
2022-04-14 23:50:06 +0700

    i

--- html/the_band/assets/js/script.js
+++ html/the_band/assets/js/script.js
@@ -1,31 +1,21 @@
-
 $(function () {
   changeSlider2();
   mobileMenu();
-  
-    
-})
-
-
-
+});
 
 function changeSlider2() {
-     const $sliders = $(".my-slider");
-     setInterval(function () {
-         const $currentSlider = $(".my-slider.active");
-         let $nextSlider = $currentSlider.next();
-         if ($nextSlider.length==0) { 
-             $nextSlider = $currentSlider.prevAll().last();
-         }
-        $currentSlider.removeClass("active");
-       $nextSlider.addClass("active");
-      
-     }, 4000);
-
+  const $sliders = $(".my-slider");
+  setInterval(function () {
+    const $currentSlider = $(".my-slider.active");
+    let $nextSlider = $currentSlider.next();
+    if ($nextSlider.length == 0) {
+      $nextSlider = $currentSlider.prevAll().last();
+    }
+    $currentSlider.removeClass("active");
+    $nextSlider.addClass("active");
+  }, 4000);
 }
 
-
- 
 function changeSlider() {
   const $sliders = $(".my-slider");
   let index = 0;
@@ -56,30 +46,27 @@ function mobileMenu() {
   //Đóng mở mobile menu
 
   $mobileMenu.on("click", function () {
-  let isClose = $header.height() === headerHeight;
-  if (isClose) {
-    $header.css({
-      height: "auto",
-    });
-  } else {
-    $header.css({
-    height: `${headerHeight}`,
-    });
-  }
+    let isClose = $header.height() === headerHeight;
+    if (isClose) {
+      $header.css({
+        height: "auto",
+      });
+    } else {
+      $header.css({
+        height: `${headerHeight}`,
+      });
+    }
   });
 
   //Tự động đóng khi chọn menu
 
-  const $menuItems = $('.nav>li');
-  for (let i = 1; i < $menuItems.length; i++){
+  const $menuItems = $(".nav>li");
+  for (let i = 1; i < $menuItems.length; i++) {
     let menuItem = $($menuItems[i]);
-    menuItem.on('click', function (item) {
-       $header.css({
-         height: `${headerHeight}`,
-       });
-    })
+    menuItem.on("click", function (item) {
+      $header.css({
+        height: `${headerHeight}`,
+      });
+    });
   }
-
-  
 }
-
